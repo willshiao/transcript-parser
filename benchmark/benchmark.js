cffd41410b3440c5b2b00a846e6b61eb832dfefb
William Shiao <willshiao@gmail.com>
2016-09-06 20:58:07 -0700

    tests: use the "benchmark" module
    Use the "benchmark" module from NPM to perform more accurate benchmarks.

--- benchmark/benchmark.js
+++ benchmark/benchmark.js
@@ -3,102 +3,104 @@
 /***********************
  * Dependencies
  ***********************/
-const Promise = require('bluebird');
-const fs = Promise.promisifyAll(require('fs'));
+const fs = require('fs');
 const path = require('path');
-const _ = require('lodash');
-const TranscriptParser = require('../app.js');
+const Benchmark = require('benchmark');
 const Readable = require('stream').Readable;
 
+const TranscriptParser = require('../app.js');
+
+
+/***********************
+ * Intialization
+ ***********************/
 const tp = new TranscriptParser();
+const suite = new Benchmark.Suite();
 
-const longTranscript = readTranscriptSync('long');
 const firstTranscript = readTranscriptSync('1');
-const numTests = 5;
+const longTranscript = readTranscriptSync('long');
 
-var s = new Readable();
+let firstStream = makeStringStream(firstTranscript);
+let longStream = makeStringStream(longTranscript);
+
+
 /***********************
  * Benchmarks
  ***********************/
-console.log('Sync Parse #1:', timeFunction(() => {
-  tp.parseOneSync(firstTranscript);
-}));
+suite
+  .add('Synchronous Short Parse', {
+    defer: false,
+    fn: () => {
+      tp.parseOneSync(firstTranscript);
+    }
+  })
+  .add('Synchronous Long Parse', {
+    defer: false,
+    fn: () => {
+      tp.parseOneSync(longTranscript);
+    }
+  })
+  .add('Asynchronous Short Parse', {
+    defer: true,
+    fn: defer => {
+      tp.parseOne(firstTranscript, () => defer.resolve());
+    }
+  })
+  .add('Asynchronous Long Parse', {
+    defer: true,
+    fn: defer => {
+      tp.parseOne(longTranscript, () => defer.resolve());
+    }
+  })
+  .add('Stream Short Parse', {
+    defer: true,
+    fn: defer => {
+      tp.parseStream(firstStream, a => {
+        //Creating the new Readable stream increases time slightly,
+        // but not by too much (< 1 ms on my machine).
+        firstStream = makeStringStream(firstTranscript);
+        defer.resolve();
+      });
+    }
+  })
+  .add('Stream Long Parse', {
+    defer: true,
+    fn: defer => {
+      tp.parseStream(longStream, a => {
+        //Creating the new Readable stream increases time slightly,
+        // but not by too much (< 1 ms on my machine).
+        longStream = makeStringStream(longTranscript);
+        defer.resolve();
+      });
+    }
+  })
+  .on('complete', function() {
+    this.forEach(printStats);
+  })
+  .run();
 
-console.log('Sync Parse #2:', timeFunction(() => {
-  tp.parseOneSync(longTranscript);
-}));
-
-console.log('Sync Parse #3:', timeFunction(() => {
-  for(var i = 0; i < numTests; i++) {
-    tp.parseOneSync(longTranscript);
-  }
-}));
-
-
-timePromise(() => {
-  return tp.parseOne(firstTranscript);
-}).then(msg => {
-  console.log('Async Parse #1:', msg);
-  return timePromise(() => tp.parseOne(longTranscript));
-}).then(msg => {
-  console.log('Async Parse #2:', msg);
-  return timePromise(parseLongPromise);
-}).then(msg => {
-  console.log('Async Parse #3:', msg);
-  s.push(firstTranscript, 'utf8');
-  s.push(null);
-  return timePromise(() => Promise.fromCallback(cb => tp.parseStream(s, cb)));
-}).then(msg => {
-  console.log('Stream Parse #1:', msg);
-  s = new Readable();
-  s.push(longTranscript, 'utf8');
-  s.push(null);
-  return timePromise(() => Promise.fromCallback(cb => tp.parseStream(s, cb)));
-}).then(msg => {
-  console.log('Stream Parse #2:', msg);
-}).catch(e => console.error(e.stack));
 
 /***********************
  * Functions
  ***********************/
-function parseLongPromise() {
-  const q = new Array(numTests);
-  for(var i = 0; i < numTests; i++) {
-    q[i] = tp.parseOne(longTranscript).then(() => {});
-  }
-  const tasks = _.chunk(q, 5);
-  return Promise.each(tasks, taskGroup => {
-    return Promise.all(taskGroup);
-  });
+function printStats(benchmark) {
+  console.log('====================');
+  console.log('Name:', benchmark.name);
+  console.log('Mean:', benchmark.stats.mean, 's');
+  console.log('Deviation:', benchmark.stats.deviation, 's');
+  console.log('Margin of error:', benchmark.stats.moe, 's');
+  console.log('Relative margin of error:', benchmark.stats.rme, '%');
+  console.log('Operations/sec:', benchmark.hz);
 }
 
-function timeAsyncFunction(func, cb) {
-  const start = process.hrtime();
-  func(() => {
-    const end = process.hrtime(start);
-    return cb(null, end[0] + 's ' + (end[1] / 1000000) + 'ms');
-  });
-}
-
-function timePromise(promise) {
-  const start = process.hrtime();
-  return promise().then(() => {
-    const end = process.hrtime(start);
-    return Promise.resolve(end[0] + 's ' + (end[1] / 1000000) + 'ms');
-  });
-}
-
-function timeFunction(func) {
-  const start = process.hrtime();
-  func();
-  const end = process.hrtime(start);
-  return end[0] + 's ' + (end[1] / 1000000) + 'ms';
-}
-
-function readTranscript(name) {
-  return fs.readFileAsync(path.join(__dirname, `/transcripts/${name}.txt`), {encoding: 'utf8'});
+function makeStringStream(str) {
+  const r = new Readable();
+  r.push(str);
+  r.push(null);
+  return r;
 }
 
 function readTranscriptSync(name) {
-  return fs.readFileSync(path.join(__dirname, `/transcripts/${name}.txt`), {encoding: 'utf8'});
+  return fs.readFileSync(path.join(__dirname, `/transcripts/${name}.txt`),
+    {encoding: 'utf8'});
 }
